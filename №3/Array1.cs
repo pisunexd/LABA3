2bf70be3e52a6c2321952202af40c8656a2abb98
az <az>
2024-11-26 02:16:29 +0300

    Добавьте файлы проекта.

---
+++ №3/Array1.cs
@@ -0,0 +1,14 @@
+﻿// Ввод числа N
+int N = int.Parse(Console.ReadLine());
+// Создаем массив длиной N
+int[] arr = new int[N];
+// Заполняем массив нечетными числами
+for (int i = 0; i < N; i++)
+{
+    arr[i] = 2 * i + 1; // нечетные числа: 1, 3, 5...
+}
+// Вывод массива
+for (int i = 0; i < N; i++)
+{
+    Console.Write(arr[i] + " ");
+}
---
+++ №3/Matrix57.cs
@@ -0,0 +1,30 @@
+﻿int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+int[,] matrix = new int[M, N];
+// Вводим элементы матрицы
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+// Меняем левую верхнюю и правую нижнюю четверти
+for (int i = 0; i < M / 2; i++)
+{
+    for (int j = 0; j < N / 2; j++)
+    {
+        int temp = matrix[i, j];
+        matrix[i, j] = matrix[i + M / 2, j + N / 2];
+        matrix[i + M / 2, j + N / 2] = temp;
+    }
+}
+// Выводим измененную матрицу
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
